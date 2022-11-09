eb8855340bf7b277ab337246283122070a28b49b
Nabila Adinda <nabilaadinda23@gmail.com>
2022-11-09 20:43:51 +0700

    Membuat database

--- lib/models/dairy.dart
+++ lib/models/dairy.dart
@@ -2,11 +2,11 @@ import 'package:nanoid/non_secure.dart';
 
 class Dairy {
   late String id;
-  String name;
-  String location;
-  String date;
-  String dairy;
-  String pic;
+  late String name;
+  late String location;
+  late String date;
+  late String dairy;
+  late String pic;
 
   Dairy({
     required this.name,
@@ -16,5 +16,25 @@ class Dairy {
     required this.pic,
   }) {
     id = nanoid(20);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'location': location,
+      'date': date,
+      'dairy': dairy,
+      'pic': pic,
+    };
+  }
+
+  Dairy.fromMap(Map<String, dynamic> map) {
+    id = map['id'].toString();
+    name = map['name'].toString();
+    location = map['location'].toString();
+    date = map['date'].toString();
+    dairy = map['dairy'].toString();
+    pic = map['pic'].toString();
   }
 }
---
+++ lib/viewModels/database/database_helper.dart
@@ -0,0 +1,86 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:hiking_dairy/models/dairy.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _databaseHelper;
+  late Database _database;
+  final String _tabelDairy = 'dairy';
+
+  DatabaseHelper._internal() {
+    _databaseHelper = this;
+  }
+
+  factory DatabaseHelper() => _databaseHelper ?? DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    _database = await _initializeDb();
+    return _database;
+  }
+
+  Future<Database> _initializeDb() async {
+    var db = await openDatabase(
+      join(await getDatabasesPath(), 'task_db.db'),
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $_tabelDairy (
+            id TEXT PRIMARY KEY, 
+            name TEXT
+            location TEXT
+            date TEXT
+            dairy TEXT
+            pic TEXT
+          )
+        ''');
+      },
+      onUpgrade: (db, oldVersion, newVersion) async {
+        await db.execute('DROP TABLE $_tabelDairy');
+        await db.execute('''
+          CREATE TABLE $_tabelDairy (
+            id TEXT PRIMARY KEY, 
+            name TEXT
+            location TEXT
+            date TEXT
+            dairy TEXT
+            pic TEXT
+            status BOOL,
+          )
+        ''');
+      },
+      version: 1,
+    );
+    return db;
+  }
+
+  Future<void> insertTask(Dairy value) async {
+    final Database db = await database;
+    await db.insert(_tabelDairy, value.toMap());
+  }
+
+  Future<List<Dairy>> getTasks() async {
+    final Database db = await database;
+    List<Map<String, dynamic>> result = await db.query(_tabelDairy);
+    return result.map((dairyMap) => Dairy.fromMap(dairyMap)).toList();
+  }
+
+  Future<bool> updateTask(Dairy value) async {
+    final Database db = await database;
+    final result = await db.update(
+      _tabelDairy,
+      value.toMap(),
+      where: 'id = ?',
+      whereArgs: [value.id],
+    );
+    return result > 0 ? true : false;
+  }
+
+  Future<bool> deletTask(Dairy value) async {
+    final Database db = await database;
+    final result = await db.delete(
+      _tabelDairy,
+      where: 'id = ?',
+      whereArgs: [value.id],
+    );
+    return result > 0 ? true : false;
+  }
+}
